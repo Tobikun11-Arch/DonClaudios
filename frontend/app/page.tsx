2f0d8d80fe958c2d138faf333bcf7759a641229f
Joenel Sevellejo <joenelsevellejo831@gmail.com>
2026-04-01 00:13:00 +0800

    basic setup o/featured based architecture

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link"
 
 export default function Home() {
  return (
   <main>
     <h1>Welcome to main route</h1>
-    <Link href="/sign-up">Sign Up</Link>
-    <Link href="/sign-in">Sign In</Link>
   </main>
  )
 }
