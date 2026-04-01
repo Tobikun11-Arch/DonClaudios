576a75dab133e288f0db1bf9f93784c32dabe541
Joenel Sevellejo <joenelsevellejo831@gmail.com>
2026-04-01 22:57:18 +0800

    users layout fixed

--- frontend/app/(customer)/layout.tsx
+++ frontend/app/(customer)/layout.tsx
@@ -1,1 +1,7 @@
 //customer auth guard before redirecting to the page
+
+export default function CustomerLayout() {
+  return (
+    <div>CustomerLayout</div>
+  )
+}
--- frontend/app/(owner)/layout.tsx
+++ frontend/app/(owner)/layout.tsx
@@ -1,1 +1,9 @@
 // Owner auth guard before redirecting to the page
+
+import React from 'react'
+
+export default function OwnerLayout() {
+  return (
+    <div>OwnerLayout</div>
+  )
+}
